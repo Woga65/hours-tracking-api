3b711b6730c81896978967cadb0e45d5fb0edc99
Woga65 <woga@mytsa.de>
2024-01-27 20:12:51 +0100

    Add user table. Add api_token table and relate it to the user table. Add a script to generate legit looking tokens.

---
+++ migrations/Version20240127185702.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240127185702 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD uid VARCHAR(255) NOT NULL, ADD name VARCHAR(255) NOT NULL, ADD vkey VARCHAR(255) DEFAULT NULL, ADD verified TINYINT(1) DEFAULT 0 NOT NULL, ADD timestamp TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP uid, DROP name, DROP vkey, DROP verified, DROP timestamp');
+    }
+}
